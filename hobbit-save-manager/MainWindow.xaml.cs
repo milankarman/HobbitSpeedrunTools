3591f0b2d69da0137067add4997b33b2dd71846f
Milan Karman <milankarman@gmail.com>
2021-10-21 04:00:21 +0200

    Added initial UI functionality

--- hobbit-save-manager/MainWindow.xaml.cs
+++ hobbit-save-manager/MainWindow.xaml.cs
@@ -1,28 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
 
 namespace hobbit_save_manager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string hobbitSaveDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "The Hobbit");
+        private string applicationSaveDir = Path.Join(".", "save-collections");
+   
         public MainWindow()
         {
             InitializeComponent();
+            InitSaveCollections();
+        }
+
+        public void InitSaveCollections()
+        {
+            Trace.WriteLine("Test");
+
+            if (!Directory.Exists(hobbitSaveDir))
+            {
+                MessageBox.Show($"The Hobbit saves folder not found at: {hobbitSaveDir}");
+                Application.Current.Shutdown();
+            }
+
+            if (!Directory.Exists(applicationSaveDir))
+            {
+                MessageBox.Show($"Application saves folder not found at: {applicationSaveDir}");
+                Application.Current.Shutdown();
+            }
+
+            foreach (string saveCollection in Directory.GetDirectories(applicationSaveDir, "*", SearchOption.TopDirectoryOnly))
+            {
+                DirectoryInfo info = new DirectoryInfo(saveCollection);
+                cbxSaveCollections.Items.Add(info.Name);
+            }
+        }
+
+        public void InitSaves(string saveCollection)
+        {
+            cbxSaves.Items.Clear();
+
+            foreach (string save in Directory.GetFiles(Path.Join(applicationSaveDir, saveCollection)))
+            {
+                cbxSaves.Items.Add(save);
+            }
+        }
+
+        private void cbxSaveCollections_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            InitSaves(cbxSaveCollections.Text);
         }
     }
 }
